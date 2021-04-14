0edb5b76e0f8a87995887a1fd40c4cd52673f2a2
甘春雨 <keyboard3@icloud.com>
2021-04-15 01:11:54 +0800

    回调支持结果是promise

--- promise.js
+++ promise.js
@@ -5,6 +5,7 @@ function Promise(executor) {
   this.result = null;
   this.error = null;
   var that = this;
+  executor(resolve, reject);
   function resolve(result) {
     that.result = result;
     that.status = "completed";
@@ -17,66 +18,55 @@ function Promise(executor) {
     if (that.notifyError) that.notifyError(); //出现异常，告诉下一个出现异常了
     else;//首次处理完，等待延迟触发
   }
-  executor(resolve, reject);
 }
-/**
- * 注册父级结果和异常订阅
- * @param {*} resultSubscriber 知道怎么处理结果
- * @param {*} errorSubscriber 知道怎么处理异常
- * @returns 
- */
+//注册父级结果和异常订阅
 Promise.prototype.register = function (resultSubscriber, errorSubscriber) {
   var parentPromise = this;
   var nextPromise = new Promise((resolve, reject) => {
     switch (parentPromise.status) {
       case "pending": //后续都没有处理，告诉前一个Promise，有结果通知我，正常和异常我都需要
-        //告诉前面，正常的通过它来通知我。
-        parentPromise.notifyResult = () => setTimeout(() => {
-          try {
-            //我要异步处理
-            if (!resultSubscriber) return resolve(parentPromise.result); //我无法处理，但是我可以告诉下一个
-            var val = resultSubscriber(parentPromise.result); //我可以处理
-            resolve(val);//处理完告诉下一个
-          } catch (error) {
-            reject(error);
-          }
-        });
-        //告诉前面，异常的通过它来通知我
-        parentPromise.notifyError = () => setTimeout(() => {
-          try {
-            //我要异步处理
-            if (!errorSubscriber) return reject(parentPromise.error); //我无法处理异常，但是我可以告诉下一个
-            var val = errorSubscriber(parentPromise.error); //我可以处理异常
-            resolve(val);//将处理结果告诉下一个
-          } catch (error) {
-            reject(error);
-          }
-        });
+        parentPromise.notifyResult = notifyResult; //告诉前面，正常的通过它来通知我。
+        parentPromise.notifyError = notifyError; //告诉前面，异常的通过它来通知我
         break;
       case "completed":  //首次处理完, 延迟触发。一个目的是等待整个通知链路完成，开始从头通知
-        setTimeout(() => {
-          try {
-            //我要异步处理
-            if (!resultSubscriber) return resolve(val); //我无法处理，直接告诉下一个
-            var val = resultSubscriber(parentPromise.result); //我可以处理，处理完结果告诉下一个
-            resolve(val);
-          } catch (error) {
-            reject(error);
-          }
-        });
+        notifyResult();
         break;
       case "failed": //首次处理完, 延迟触发。一个目的是等待整个通知链路完成，开始从头通知
-        setTimeout(() => {
-          try {
-            //我要异步处理
-            if (!errorSubscriber) return reject(error); //我无法处理异常，直接告诉下一个
-            var val = errorSubscriber(parentPromise.error); //我可以处理异常，处理完结果告诉下一个
-            resolve(val);
-          } catch (error) {
-            reject(error);
-          }
-        });
+        notifyError();
         break;
+    }
+    function notifyResult() {
+      if (!resultSubscriber) return resolvePromiseVal(parentPromise.result); //我无法处理，但是我可以告诉下一个
+      asyncRunResultSubscriber();
+    }
+    function notifyError() {
+      if (!errorSubscriber) return reject(parentPromise.error); //我无法处理异常，但是我可以告诉下一个
+      asyncRunErrorSubscriber();
+    }
+    function asyncRunResultSubscriber() {
+      setTimeout(function () {
+        try {
+          var val = resultSubscriber(parentPromise.result); //我可以处理，处理完结果告诉下一个
+          resolvePromiseVal(val);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    }
+    function asyncRunErrorSubscriber() {
+      setTimeout(function () {
+        try {
+          var val = errorSubscriber(parentPromise.error); //我可以处理异常，处理完结果告诉下一个
+          resolvePromiseVal(val);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    }
+    function resolvePromiseVal(value) {
+      if (value && (typeof value === 'object' && value instanceof Promise)) {
+        value.register(function (val) { resolve(val) }, function (error) { reject(error) });
+      } else resolve(value);
     }
   });
   return nextPromise;
@@ -87,5 +77,4 @@ Promise.prototype.then = function (resultSubscriber) {
 Promise.prototype.catch = function (errorSubscriber) {
   return this.register(null, errorSubscriber);
 }
-
 module.exports = Promise
--- test.js
+++ test.js
@@ -1,17 +1,22 @@
 const Promise = require("./promise.js");
-console.log("Promise",Promise)
 //测试Promise
 new Promise((resolve, reject) => {
   console.log(0);
-  resolve(0);
+  resolve(10);
 }).then((val) => {
-  val++;
-  console.log(val);
-  throw "错误了"
-  return val;
-}).catch((err) => {
-  console.log("err", err);
-  return "ok"
+  return new Promise((resolve, reject) => {
+    console.log("new Promise")
+    setTimeout(() => {
+      console.log("new Promise setTimeout", val)
+      resolve(val + 3);
+    }, 1000);
+  }).then((val2) => {
+    throw "new promise error"
+    console.log("new Promise then", val2)
+    return val2;
+  });
 }).then((val) => {
-  console.log(val);
+  console.log("end" + val);
+}).catch(err => {
+  console.log("catch end error", err)
 });
